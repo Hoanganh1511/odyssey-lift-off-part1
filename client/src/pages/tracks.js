4bc342d0bd4a05c891a7caebe1d79066db1edd50
Hoang Tuan Anh <anhht.fe@gmail.com>
2025-05-13 17:23:22 +0700

    update

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Layout } from '../components';
-
+import React from "react";
+import { Layout } from "../components";
+import { useQuery, gql } from "@apollo/client";
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+const SPACECATS = gql`
+  query ListSpaceCats {
+    spaceCats {
+      name
+      age
+      missions {
+        name
+        description
+      }
+    }
+  }
+`;
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(SPACECATS);
+  return <Layout grid>{JSON.stringify(data)} 21312 </Layout>;
 };
 
 export default Tracks;
